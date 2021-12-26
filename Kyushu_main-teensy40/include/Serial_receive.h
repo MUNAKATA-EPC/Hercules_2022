69514f641b4430f322999fb925070ceec5845c59
nakamoto800 <tatsuyanakamoto1015@gmail.com>
2021-12-26 12:43:19 +0900

    スペースが増えたよ！(見づらくて草)

--- Kyushu_main-teensy40/include/Serial_receive.h
+++ Kyushu_main-teensy40/include/Serial_receive.h
@@ -1,18 +1,26 @@
 void Serial_receive() {
+
   if (Serial3.available() > 0) {
+
     head_USS = Serial3.read();
 
     if (head_USS == 255) {
+
       USS = Serial3.read();
       USS1 = Serial3.read();
       USS2 = Serial3.read();
       USS3 = Serial3.read();
       USS4 = Serial3.read();
+
     }
+
   }
 
   if (Serial4.available() > 0) {
+
     IMU = Serial4.read();
     IMU = IMU * 2;
+
   }
+  
 }
