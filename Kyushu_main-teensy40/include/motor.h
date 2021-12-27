9ce2b9e6d9857994a0a58b770c909d765072cb57
nakamoto800 <tatsuyanakamoto1015@gmail.com>
2021-12-27 18:36:24 +0900

    PID設定してね

--- Kyushu_main-teensy40/include/motor.h
+++ Kyushu_main-teensy40/include/motor.h
@@ -1,1 +1,80 @@
-int motor1
+void Motor_1() {  //停止
+  motor_1 = 0;
+  motor_2 = 0;
+  motor_3 = 0;
+  motor_4 = 0;
+
+  dsr1202.move(G, H, I, J);
+}
+
+void Motor_2() {  //前
+  motor_1 = 0;
+  motor_2 = 0;
+  motor_3 = 0;
+  motor_4 = 0;
+
+  dsr1202.move(G, H, I, J);
+}
+
+void Motor_3() {  //後
+  motor_1 = 0;
+  motor_2 = 0;
+  motor_3 = 0;
+  motor_4 = 0;
+
+  dsr1202.move(G, H, I, J);
+}
+
+void Motor_4() {  //左
+  motor_1 = 0;
+  motor_2 = 0;
+  motor_3 = 0;
+  motor_4 = 0;
+
+  dsr1202.move(G, H, I, J);
+}
+
+void Motor_5() {  //右
+  motor_1 = 0;
+  motor_2 = 0;
+  motor_3 = 0;
+  motor_4 = 0;
+
+  dsr1202.move(G, H, I, J);
+}
+
+void Motor_6() {  //左前
+  motor_1 = 0;
+  motor_2 = 0;
+  motor_3 = 0;
+  motor_4 = 0;
+
+  dsr1202.move(G, H, I, J);
+}
+
+void Motor_7() {  //右前
+  motor_1 = 0;
+  motor_2 = 0;
+  motor_3 = 0;
+  motor_4 = 0;
+
+  dsr1202.move(G, H, I, J);
+}
+
+void Motor_8() {  //左後
+  motor_1 = 0;
+  motor_2 = 0;
+  motor_3 = 0;
+  motor_4 = 0;
+
+  dsr1202.move(G, H, I, J);
+}
+
+void Motor_9() {  //右後
+  motor_1 = 0;
+  motor_2 = 0;
+  motor_3 = 0;
+  motor_4 = 0;
+
+  dsr1202.move(G, H, I, J);
+}
