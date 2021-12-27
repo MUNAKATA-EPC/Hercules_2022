e9735e8cba0bfa53fcdd47b7e60f323bf1c5363b
nakamoto800 <tatsuyanakamoto1015@gmail.com>
2021-12-27 18:37:19 +0900

    スペースが減ったよ

--- Kyushu_main-teensy40/include/Serial_receive.h
+++ Kyushu_main-teensy40/include/Serial_receive.h
@@ -1,48 +1,32 @@
 void Serial_receive() {
-
   if (Serial2.available() > 0) {
-
     head_CAM = Serial2.read();
 
     if (head_CAM == 255) {
-
       CAM_angle = Serial2.read();
       CAM_distance = Serial2.read();
-
     }
-    
   }
 
   if (Serial3.available() > 0) {
-
     head_USS = Serial3.read();
 
     if (head_USS == 255) {
-
       USS = Serial3.read();
       USS1 = Serial3.read();
       USS2 = Serial3.read();
       USS3 = Serial3.read();
       USS4 = Serial3.read();
-
     }
-
   }
 
   if (Serial4.available() > 0) {
-
     IMU = Serial4.read();
     
     if (IMU <= 90) {
-
       IMU = IMU + 90;
-
     } else {
-
       IMU = IMU - 90;
-      
     }
-    
   }
-  
 }
