a7beeaab27fcf0a7c93a1ab603ab1f3ea057a25a
nakamoto800 <tatsuyanakamoto1015@gmail.com>
2022-01-05 14:12:27 +0900

    Update main.cpp

--- Kyushu_main-teensy40/src/main.cpp
+++ Kyushu_main-teensy40/src/main.cpp
@@ -13,59 +13,61 @@ DSR1202 dsr1202(1);
 #include <Serial_receive.h>
 #include <print_LCD.h>
 #include <control_LED.h>
-//#include <pid.h>
-//#include <motor.h>
+#include <pid.h>
+#include <motor.h>
 
 void setup() {
   startup();
+
 }
 
 void loop() {
   Serial_receive();
   print_LCD();
   control_LED();
-  //pid();
+  pid();
 
   if (LCD.state == 4 && white.state == 1 && digitalRead(switch_program) == HIGH) {
     if (timer_start != 0.00) {   
       timer = (millis() / 100) - timer_start;
+
     }
 
     if (timer < 2.00) {
-      //Motor_1();
+      Motor_1();  //方向修正
 
     } else if (timer < 7.00) {
       if (USS == 1) {
-        //Motor_1();
+        Motor_1();  //方向修正
 
       } else if (USS == 2) {
-        /* モーター_2(前) */
+        Motor_2();  //前
 
       } else if (USS == 3) {
-        /* モーター_3(後) */
+        Motor_3();  //後
 
       } else if (USS == 4) {
-        /* モーター_4(左) */
+        Motor_4();  //左
 
       } else if (USS == 5) {
-        /* モーター_5(右) */
+        Motor_5();  //右
 
       } else if (USS == 6) {
-        /* モーター_6(左前) */
+        Motor_6();  //左前
 
       } else if (USS == 7) {
-        /* モーター_7(右前) */
+        Motor_7();  //右前
 
       } else if (USS == 8) {
-        /* モーター_8(左後) */
+        Motor_8();  //左後
 
       } else if (USS == 9) {
-        /* モーター_9(右後) */
+        Motor_9();  //右後
+
       }
-      
     } else {
       if (analogRead(LINE_ALL) < 100) {
-        Serial1.print("100R200R300R400R");
+        Serial1.print("1R0002R0003R0004R000");
         timer_start = millis() / 100;
 
       } else {
@@ -79,37 +81,37 @@ void loop() {
           if (USS3 < 45) {
             if (USS2 > 50) {
               if (USS4 > 50) {
-                //Motor_1();
+                Motor_1();  //方向修正
 
               } else {
-                /* モーター_4(左) */
-              }
+                Motor_4();  //左
 
+              }
             } else if (USS4 > 50) {
-              /* モーター_5(右) */
+              Motor_5();  //右
 
             } else {
-              //Motor_1();
-            }
+              Motor_1();  //方向修正
 
+            }
           } else if (USS2 > 50) {
             if (USS4 > 50) {
-              /* モーター_3(後) */
+              Motor_3();  //後
 
             } else {
-              /* モーター_8(左後) */
-            }
+              Motor_8();  //左後
 
+            }
           } else if (USS4 > 50) {
-            /* モーター_9(右後) */
+            Motor_9();  //右後
 
           } else {
-            /* モーター_3(後) */
+            Motor_3();  //後
+
           }
         }
       }
     }
-
   } else if (LCD.state == 5 && white.state == 1 && digitalRead(switch_program) == HIGH) {
     dsr1202.move(10, 10, 10, 10);  //右前, 右後, 左前, 左後(全部正回転で反時計回り)
 
