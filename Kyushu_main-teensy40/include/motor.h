75b7e49763e8d4510016a49d025952df9c993a70
nakamoto800 <tatsuyanakamoto1015@gmail.com>
2022-01-05 15:27:42 +0900

    コメント位置が変わりました

--- Kyushu_main-teensy40/include/motor.h
+++ Kyushu_main-teensy40/include/motor.h
@@ -3,36 +3,45 @@ void Motor_limit() {
 
   if (G < -30) {
     G = -30;
+
   } else if (G > 30) {
     G = 30;
+
   }
 
   H = A + motor_2;
 
   if (H < -30) {
     H = -30;
+
   } else if (H > 30) {
     H = 30;
+
   }
 
   I = A + motor_3;
 
   if (I < -30) {
     I = -30;
+
   } else if (I > 30) {
     I = 30;
+
   }
 
   J = A + motor_4;
 
   if (J < -30) {
     J = -30;
+
   } else if (J > 30) {
     J = 30;
+    
   }
 }
 
-void Motor_1() {  //停止
+//停止
+void Motor_1() {
   motor_1 = 0;  //右前
   motor_2 = 0;  //右後
   motor_3 = 0;  //左前
@@ -43,7 +52,8 @@ void Motor_1() {  //停止
   dsr1202.move(G, H, I, J);
 }
 
-void Motor_2() {  //前
+//前
+void Motor_2() {
   motor_1 = 20; //右前
   motor_2 = 20; //右後
   motor_3 = -20;  //左前
@@ -54,7 +64,8 @@ void Motor_2() {  //前
   dsr1202.move(G, H, I, J);
 }
 
-void Motor_3() {  //後
+//後
+void Motor_3() {
   motor_1 = -20;  //右前
   motor_2 = -20;  //右後
   motor_3 = 20; //左前
@@ -65,7 +76,8 @@ void Motor_3() {  //後
   dsr1202.move(G, H, I, J);
 }
 
-void Motor_4() {  //左
+//左
+void Motor_4() {
   motor_1 = 20; //右前
   motor_2 = -20;  //右後
   motor_3 = 20; //左前
@@ -76,7 +88,8 @@ void Motor_4() {  //左
   dsr1202.move(G, H, I, J);
 }
 
-void Motor_5() {  //右
+//右
+void Motor_5() {
   motor_1 = -20;  //右前
   motor_2 = 20; //右後
   motor_3 = -20;  //左前
@@ -87,7 +100,8 @@ void Motor_5() {  //右
   dsr1202.move(G, H, I, J);
 }
 
-void Motor_6() {  //左前
+//左前
+void Motor_6() {
   motor_1 = 20; //右前
   motor_2 = 0;  //右後
   motor_3 = 0;  //左前
@@ -98,7 +112,8 @@ void Motor_6() {  //左前
   dsr1202.move(G, H, I, J);
 }
 
-void Motor_7() {  //右前
+//右前
+void Motor_7() {
   motor_1 = 0;  //右前
   motor_2 = 20; //右後
   motor_3 = -20;  //左前
@@ -109,7 +124,8 @@ void Motor_7() {  //右前
   dsr1202.move(G, H, I, J);
 }
 
-void Motor_8() {  //左後
+//左後
+void Motor_8() {
   motor_1 = 0;  //右前
   motor_2 = -20;  //右後
   motor_3 = 20; //左前
@@ -120,7 +136,8 @@ void Motor_8() {  //左後
   dsr1202.move(G, H, I, J);
 }
 
-void Motor_9() {  //右後
+//右後
+void Motor_9() {
   motor_1 = -20;  //右前
   motor_2 = 0;  //右後
   motor_3 = 0;  //左前
