310e11cbfbbd2d7340691f802844151f00a5159b
nakamoto800 <tatsuyanakamoto1015@gmail.com>
2022-01-05 15:29:31 +0900

    クラスの表記がまとまったり、ゲイン値が消えたり

--- Kyushu_main-teensy40/include/variable.h
+++ Kyushu_main-teensy40/include/variable.h
@@ -18,19 +18,14 @@ public:
   int val = 0;
   int old_val = 0;
   int state = 0;
+  
 };
 
-Class1 LCD;
-Class1 white;
+Class1 LCD, white;
 
 double timer;
 double timer_start = 0.00;
 
-double Y = 90; //ジャイロ中心値
-double K = 10;  //比例ゲイン
-double L = 0.8; //積分ゲイン
-double M = 19;  //微分ゲイン
-double O = 30; //操作分母量
 double W, V = 0;
 double X, Z = 0;
 double A, B, C = 0;
