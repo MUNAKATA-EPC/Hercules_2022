89dc0ba5dd0de6e66717673a7afc42b099e12194
nakamoto800 <tatsuyanakamoto1015@gmail.com>
2022-01-06 17:12:24 +0900

    変数被り防止とメモリ使用量削減

--- Kyushu_main-teensy40/include/pid_parameter.h
+++ Kyushu_main-teensy40/include/pid_parameter.h
@@ -1,5 +1,5 @@
-double Y = 90; //ジャイロ中心値
-double K = 10;  //比例ゲイン
-double L = 0.8; //積分ゲイン
-double M = 19;  //微分ゲイン
-double O = 30; //操作分母量
+#define IMU_center 90 //ジャイロ中心値
+#define gain_P 10  //比例ゲイン
+#define gain_I 0.8 //積分ゲイン
+#define gain_D 19  //微分ゲイン
+#define operation_PID 30 //操作分母量
