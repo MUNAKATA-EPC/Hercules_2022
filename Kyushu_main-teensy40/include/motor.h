40a9c3327a668cf0f3f3ce1dfc9dcfa851819ac0
nakamoto800 <tatsuyanakamoto1015@gmail.com>
2022-01-08 21:15:20 +0900

    PID制御を一時的に外してます

--- Kyushu_main-teensy40/include/motor.h
+++ Kyushu_main-teensy40/include/motor.h
@@ -1,141 +1,206 @@
-void Motor_limit() {
-  operation_G = operation_A + motor_1;
-
-  if (operation_G < -motor_limit) {
-    operation_G = -motor_limit;
-  } else if (operation_G > motor_limit) {
-    operation_G = motor_limit;
-  }
-
-  operation_H = operation_A + motor_2;
-
-  if (operation_H < -motor_limit) {
-    operation_H = -motor_limit;
-  } else if (operation_H > motor_limit) {
-    operation_H = motor_limit;
-  }
-
-  operation_I = operation_A + motor_3;
-
-  if (operation_I < -motor_limit) {
-    operation_I = -motor_limit;
-  } else if (operation_I > motor_limit) {
-    operation_I = motor_limit;
-  }
-
-  operation_J = operation_A + motor_4;
-
-  if (operation_J < -motor_limit) {
-    operation_J = -motor_limit;
-  } else if (operation_J > motor_limit) {
-    operation_J = motor_limit;
-  }
-}
-
 //停止
 void Motor_1() {
-  motor_1 = 0;  //右前
-  motor_2 = 0;  //右後
-  motor_3 = 0;  //左前
-  motor_4 = 0;  //左後
-
-  Motor_limit();
-
-  dsr1202.move(operation_G, operation_H, operation_I, operation_J);
+  if (IMU < 23) {
+    dsr1202.move(-15, -15, -15, -15); //全方向共通値
+  } else if (IMU < 45) {
+    dsr1202.move(-10, -10, -10, -10); //全方向共通値
+  } else if (IMU < 68) {
+    dsr1202.move(-7, -7, -7, -7); //修正強
+  } else if (IMU < 88) {
+    dsr1202.move(-7, -7, -7, -7); //修正弱
+  } else if (IMU < 92) {
+    dsr1202.move(0, 0, 0, 0);
+  } else if (IMU < 113) {
+    dsr1202.move(7, 7, 7, 7); //修正弱
+  } else if (IMU < 135) {
+    dsr1202.move(7, 7, 7, 7); //修正強
+  } else if (IMU < 158) {
+    dsr1202.move(10, 10, 10, 10); //全方向共通値
+  } else {
+    dsr1202.move(15, 15, 15, 15); //全方向共通値
+  }
 }
 
 //前
 void Motor_2() {
-  motor_1 = motor_speed; //右前
-  motor_2 = motor_speed; //右後
-  motor_3 = -motor_speed;  //左前
-  motor_4 = -motor_speed;  //左後
-
-  Motor_limit();
-
-  dsr1202.move(operation_G, operation_H, operation_I, operation_J);
+  if (IMU < 23) {
+    dsr1202.move(-15, -15, -15, -15); //全方向共通値
+  } else if (IMU < 45) {
+    dsr1202.move(-10, -10, -10, -10); //全方向共通値
+  } else if (IMU < 68) {
+    dsr1202.move(15, 15, -25, -25); //修正強
+  } else if (IMU < 88) {
+    dsr1202.move(18, 18, -22, -22); //修正弱
+  } else if (IMU < 92) {
+    dsr1202.move(20, 20, -20, -20);
+  } else if (IMU < 113) {
+    dsr1202.move(22, 22, -18, -18); //修正弱
+  } else if (IMU < 135) {
+    dsr1202.move(25, 25, -15, -15); //修正強
+  } else if (IMU < 158) {
+    dsr1202.move(10, 10, 10, 10); //全方向共通値
+  } else {
+    dsr1202.move(15, 15, 15, 15); //全方向共通値
+  }
 }
 
 //後
 void Motor_3() {
-  motor_1 = -motor_speed;  //右前
-  motor_2 = -motor_speed;  //右後
-  motor_3 = motor_speed; //左前
-  motor_4 = motor_speed; //左後
-
-  Motor_limit();
-
-  dsr1202.move(operation_G, operation_H, operation_I, operation_J);
+  if (IMU < 23) {
+    dsr1202.move(-15, -15, -15, -15); //全方向共通値
+  } else if (IMU < 45) {
+    dsr1202.move(-10, -10, -10, -10); //全方向共通値
+  } else if (IMU < 68) {
+    dsr1202.move(-25, -25, 15, 15); //修正強
+  } else if (IMU < 88) {
+    dsr1202.move(-22, -22, 18, 18); //修正弱
+  } else if (IMU < 92) {
+    dsr1202.move(-20, -20, 20, 20);
+  } else if (IMU < 113) {
+    dsr1202.move(-18, -18, 22, 22); //修正弱
+  } else if (IMU < 135) {
+    dsr1202.move(-15, -15, 25, 25); //修正強
+  } else if (IMU < 158) {
+    dsr1202.move(10, 10, 10, 10); //全方向共通値
+  } else {
+    dsr1202.move(15, 15, 15, 15); //全方向共通値
+  }
 }
 
 //左
 void Motor_4() {
-  motor_1 = motor_speed; //右前
-  motor_2 = -motor_speed;  //右後
-  motor_3 = motor_speed; //左前
-  motor_4 = -motor_speed;  //左後
-
-  Motor_limit();
-
-  dsr1202.move(operation_G, operation_H, operation_I, operation_J);
+  if (IMU < 23) {
+    dsr1202.move(-15, -15, -15, -15); //全方向共通値
+  } else if (IMU < 45) {
+    dsr1202.move(-10, -10, -10, -10); //全方向共通値
+  } else if (IMU < 68) {
+    dsr1202.move(15, -25, 15, -25); //修正強
+  } else if (IMU < 88) {
+    dsr1202.move(18, -22, 18, -22); //修正弱
+  } else if (IMU < 92) {
+    dsr1202.move(20, -20, 20, -20);
+  } else if (IMU < 113) {
+    dsr1202.move(22, -18, 22, -18); //修正弱
+  } else if (IMU < 135) {
+    dsr1202.move(25, -15, 25, -15); //修正強
+  } else if (IMU < 158) {
+    dsr1202.move(10, 10, 10, 10); //全方向共通値
+  } else {
+    dsr1202.move(15, 15, 15, 15); //全方向共通値
+  }
 }
 
 //右
 void Motor_5() {
-  motor_1 = -motor_speed;  //右前
-  motor_2 = motor_speed; //右後
-  motor_3 = -motor_speed;  //左前
-  motor_4 = motor_speed; //左後
-
-  Motor_limit();
-
-  dsr1202.move(operation_G, operation_H, operation_I, operation_J);
+  if (IMU < 23) {
+    dsr1202.move(-15, -15, -15, -15); //全方向共通値
+  } else if (IMU < 45) {
+    dsr1202.move(-10, -10, -10, -10); //全方向共通値
+  } else if (IMU < 68) {
+    dsr1202.move(-25, 15, -25, 15); //修正強
+  } else if (IMU < 88) {
+    dsr1202.move(-22, 18, -22, 18); //修正弱
+  } else if (IMU < 92) {
+    dsr1202.move(-20, 20, -20, 20);
+  } else if (IMU < 113) {
+    dsr1202.move(-18, 22, -18, 22); //修正弱
+  } else if (IMU < 135) {
+    dsr1202.move(-15, 25, -15, 25); //修正強
+  } else if (IMU < 158) {
+    dsr1202.move(10, 10, 10, 10); //全方向共通値
+  } else {
+    dsr1202.move(15, 15, 15, 15); //全方向共通値
+  }
 }
 
 //左前
 void Motor_6() {
-  motor_1 = motor_speed; //右前
-  motor_2 = 0;  //右後
-  motor_3 = 0;  //左前
-  motor_4 = -motor_speed;  //左後
-
-  Motor_limit();
-
-  dsr1202.move(operation_G, operation_H, operation_I, operation_J);
+  if (IMU < 23) {
+    dsr1202.move(-15, -15, -15, -15); //全方向共通値
+  } else if (IMU < 45) {
+    dsr1202.move(-10, -10, -10, -10); //全方向共通値
+  } else if (IMU < 68) {
+    dsr1202.move(15, -7, -7, -25); //修正強
+  } else if (IMU < 88) {
+    dsr1202.move(18, -7, -7, -22); //修正弱
+  } else if (IMU < 92) {
+    dsr1202.move(20, 0, 0, -20);
+  } else if (IMU < 113) {
+    dsr1202.move(22, 7, 7, -18); //修正弱
+  } else if (IMU < 135) {
+    dsr1202.move(25, 7, 7, -15); //修正強
+  } else if (IMU < 158) {
+    dsr1202.move(10, 10, 10, 10); //全方向共通値
+  } else {
+    dsr1202.move(15, 15, 15, 15); //全方向共通値
+  }
 }
 
 //右前
 void Motor_7() {
-  motor_1 = 0;  //右前
-  motor_2 = motor_speed; //右後
-  motor_3 = -motor_speed;  //左前
-  motor_4 = 0;  //左後
-
-  Motor_limit();
-
-  dsr1202.move(operation_G, operation_H, operation_I, operation_J);
+  if (IMU < 23) {
+    dsr1202.move(-15, -15, -15, -15); //全方向共通値
+  } else if (IMU < 45) {
+    dsr1202.move(-10, -10, -10, -10); //全方向共通値
+  } else if (IMU < 68) {
+    dsr1202.move(-7, 15, -25, -7); //修正強
+  } else if (IMU < 88) {
+    dsr1202.move(-7, 18, -22, -7); //修正弱
+  } else if (IMU < 92) {
+    dsr1202.move(0, 20, -20, 0);
+  } else if (IMU < 113) {
+    dsr1202.move(7, 22, -18, 7); //修正弱
+  } else if (IMU < 135) {
+    dsr1202.move(7, 25, -15, 7); //修正強
+  } else if (IMU < 158) {
+    dsr1202.move(10, 10, 10, 10); //全方向共通値
+  } else {
+    dsr1202.move(15, 15, 15, 15); //全方向共通値
+  }
 }
 
 //左後
 void Motor_8() {
-  motor_1 = 0;  //右前
-  motor_2 = -motor_speed;  //右後
-  motor_3 = motor_speed; //左前
-  motor_4 = 0;  //左後
-
-  Motor_limit();
-
-  dsr1202.move(operation_G, operation_H, operation_I, operation_J);
+  if (IMU < 23) {
+    dsr1202.move(-15, -15, -15, -15); //全方向共通値
+  } else if (IMU < 45) {
+    dsr1202.move(-10, -10, -10, -10); //全方向共通値
+  } else if (IMU < 68) {
+    dsr1202.move(-7, -25, 15, -7); //修正強
+  } else if (IMU < 88) {
+    dsr1202.move(-7, -22, 18, -7); //修正弱
+  } else if (IMU < 92) {
+    dsr1202.move(0, -20, 20, 0);
+  } else if (IMU < 113) {
+    dsr1202.move(7, -18, 22, 7); //修正弱
+  } else if (IMU < 135) {
+    dsr1202.move(7, -15, 25, 7); //修正強
+  } else if (IMU < 158) {
+    dsr1202.move(10, 10, 10, 10); //全方向共通値
+  } else {
+    dsr1202.move(15, 15, 15, 15); //全方向共通値
+  }
 }
 
 //右後
 void Motor_9() {
-  motor_1 = -motor_speed;  //右前
-  motor_2 = 0;  //右後
-  motor_3 = 0;  //左前
-  motor_4 = motor_speed; //左後
-
-  Motor_limit();
-
-  dsr1202.move(operation_G, operation_H, operation_I, operation_J);
+  if (IMU < 23) {
+    dsr1202.move(-15, -15, -15, -15); //全方向共通値
+  } else if (IMU < 45) {
+    dsr1202.move(-10, -10, -10, -10); //全方向共通値
+  } else if (IMU < 68) {
+    dsr1202.move(-25, -7, -7, 15); //修正強
+  } else if (IMU < 88) {
+    dsr1202.move(-22, -7, -7, 18); //修正弱
+  } else if (IMU < 92) {
+    dsr1202.move(-20, 0, 0, 20);
+  } else if (IMU < 113) {
+    dsr1202.move(-18, 7, 7, 22); //修正弱
+  } else if (IMU < 135) {
+    dsr1202.move(-15, 7, 7, 25); //修正強
+  } else if (IMU < 158) {
+    dsr1202.move(10, 10, 10, 10); //全方向共通値
+  } else {
+    dsr1202.move(15, 15, 15, 15); //全方向共通値
+  }
 }
