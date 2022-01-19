b39d40d68d5f0e7226b068ae0b556b489b79fd86
nakamoto800 <tatsuyanakamoto1015@gmail.com>
2022-01-19 22:42:55 +0900

    処理の仕様変更

--- Kyushu_nakamoto-teensy40/include/Serial_receive.h
+++ Kyushu_nakamoto-teensy40/include/Serial_receive.h
@@ -17,33 +17,52 @@ void Serial_receive() {
       USS2 = Serial3.read();
       USS3 = Serial3.read();
       USS4 = Serial3.read();
-
     }
   }
 
   if (Serial4.available() > 0) {
     IMU = Serial4.read();
+  }
 
-    if (IMU < ThresholdOfIMU[0]) {
-      direction = 5;
-    } else if (IMU < ThresholdOfIMU[1]) {
-      direction = 6;
-    } else if (IMU < ThresholdOfIMU[2]) {
-      direction = 7;
-    } else if (IMU < ThresholdOfIMU[3]) {
-      direction = 8;
-    } else if (IMU < ThresholdOfIMU[4]) {
-      direction = 9;
-    } else if (IMU < ThresholdOfIMU[5]) {
-      direction = 1;
-    } else if (IMU < ThresholdOfIMU[6]) {
-      direction = 2;
-    } else if (IMU < ThresholdOfIMU[7]) {
-      direction = 3;
-    } else if (IMU < ThresholdOfIMU[8]) {
-      direction = 4;
-    } else {
-      direction = 5;
-    }
+  if (CAM_angle < 6) {
+    CAM_area = 2; //前
+  } else if (CAM_angle < 35) {
+    CAM_area = 7; //右前
+  } else if (CAM_angle < 56) {
+    CAM_area = 5; //右
+  } else if (CAM_angle < 76) {
+    CAM_area = 9; //右後
+  } else if (CAM_angle < 96) {
+    CAM_area = 3; //後
+  } else if (CAM_angle < 116) {
+    CAM_area = 8; //左後
+  } else if (CAM_angle < 136) {
+    CAM_area = 4; //左
+  } else if (CAM_angle < 176) {
+    CAM_area = 6; //左前
+  } else {
+    CAM_area = 2; //前
+  }
+
+  if (IMU < 3) {
+    direction = 5;
+  } else if (IMU < 23) {
+    direction = 6;
+  } else if (IMU < 45) {
+    direction = 7;
+  } else if (IMU < 68) {
+    direction = 8;
+  } else if (IMU < 90) {
+    direction = 9;
+  } else if (IMU < 113) {
+    direction = 1;
+  } else if (IMU < 135) {
+    direction = 2;
+  } else if (IMU < 158) {
+    direction = 3;
+  } else if (IMU < 178) {
+    direction = 4;
+  } else {
+    direction = 5;
   }
 }
