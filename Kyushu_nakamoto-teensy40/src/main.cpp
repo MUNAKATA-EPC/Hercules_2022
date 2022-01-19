f297ec3461fc2a917b9a85f9d35d788ba635e929
nakamoto800 <tatsuyanakamoto1015@gmail.com>
2022-01-19 17:13:12 +0900

    走り抜けるぜッ！最後まで

--- Kyushu_nakamoto-teensy40/src/main.cpp
+++ Kyushu_nakamoto-teensy40/src/main.cpp
@@ -25,13 +25,13 @@ void loop() {
   control_LED();
 
   if (LCD.state == 4 && white.state == 1 && digitalRead(switch_program) == HIGH) {
-    if (timer_start != 0.00) {   
-      timer = (millis() / 100) - timer_start;
+    if (timer_start != 0) {   
+      timer = millis() - timer_start;
     }
     
-    if (timer < 2.00) {
+    if (timer < 200) {
       Motor_1();  //方向修正
-    } else if (timer < 7.00) {
+    } else if (timer < 700) {
       if (USS == 1) {
         Motor_1();  //方向修正
       } else if (USS == 2) {
@@ -52,37 +52,69 @@ void loop() {
         Motor_9();  //右後
       }
     } else {
-      if (analogRead(LINE_ALL) < 100) {
+      if (analogRead(LINE_ALL) < 300) {
         Serial1.println("1R0002R0003R0004R000");
-        timer_start = millis() / 100;
+        timer_start = millis();
       } else {
-        if (CAM_angle < 120) {
-          /* code */
-        } else if (CAM_angle < 240) {
-          /* code */
+        if (CAM_distance < 70) {
+          if (CAM_angle < 6) {
+            Motor_2();  //前進
+          } else if (CAM_angle < 35) {
+            Motor_7();  //右前
+          } else if (CAM_angle < 56) {
+            Motor_3();  //右
+          } else if (CAM_angle < 76) {
+            Motor_8();  //右後
+          } else if (CAM_angle < 96) {
+            Motor_4();  //後
+          } else if (CAM_angle < 116) {
+            Motor_9();  //左後
+          } else if (CAM_angle < 136) {
+            Motor_3();  //左
+          } else if (CAM_angle < 176) {
+            Motor_6();  //左前
+          }
         } else {
-          if (USS3 < 45) {
-            if (USS2 > 50) {
-              if (USS4 > 50) {
-                Motor_1();  //方向修正
+          if (CAM_angle < 6) {
+            Motor_2();  //前進
+          } else if (CAM_angle < 35) {
+            Motor_7();  //右前
+          } else if (CAM_angle < 56) {
+            Motor_5();  //右
+          } else if (CAM_angle < 76) {
+            Motor_9();  //右後
+          } else if (CAM_angle < 96) {
+            Motor_3();  //後
+          } else if (CAM_angle < 116) {
+            Motor_8();  //左後
+          } else if (CAM_angle < 136) {
+            Motor_4();  //左
+          } else if (CAM_angle < 176) {
+            Motor_6();  //左前
+          } else {
+            if (USS3 < 45) {
+              if (USS2 > 50) {
+                if (USS4 > 50) {
+                  Motor_1();  //方向修正
+                } else {
+                  Motor_4();  //左
+                }
+              } else if (USS4 > 50) {
+                Motor_5();  //右
               } else {
-                Motor_4();  //左
+                Motor_1();  //方向修正
+              }
+            } else if (USS2 > 50) {
+              if (USS4 > 50) {
+                Motor_3();  //後
+              } else {
+                Motor_8();  //左後
               }
             } else if (USS4 > 50) {
-              Motor_5();  //右
+              Motor_9();  //右後
             } else {
-              Motor_1();  //方向修正
-            }
-          } else if (USS2 > 50) {
-            if (USS4 > 50) {
               Motor_3();  //後
-            } else {
-              Motor_8();  //左後
             }
-          } else if (USS4 > 50) {
-            Motor_9();  //右後
-          } else {
-            Motor_3();  //後
           }
         }
       }
