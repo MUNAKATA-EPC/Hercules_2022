de0db11d0b487238542b768466a300608e86bce6
Tatsuya Nakamoto <tatsuyanakamoto1015@gmail.com>
2022-03-10 15:30:51 +0900

    Startupをmain内に移動

--- Japan_test-teensy40/src/main.cpp
+++ Japan_test-teensy40/src/main.cpp
@@ -8,7 +8,6 @@ DSR1202 dsr1202(1);
 
 #include "define.h"
 #include "variable.h"
-#include "startup.h"
 #include "Serial_receive.h"
 #include "print_LCD.h"
 #include "control_LED.h"
@@ -17,7 +16,65 @@ DSR1202 dsr1202(1);
 #include "motor.h"
 
 void setup() {
-  startup();
+  pinMode(button_LCD, INPUT);
+  pinMode(switch_program, INPUT);
+  pinMode(button_white, INPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(LINE_1, INPUT);
+  pinMode(LINE_2, INPUT);
+  pinMode(LINE_3, INPUT);
+  pinMode(LINE_4, INPUT);
+
+  lcd.begin(); //LCDの準備開始
+
+  lcd.setCursor(0, 0);
+  lcd.print("System");
+  lcd.setCursor(0, 1);
+  lcd.print("booting");
+
+  dsr1202.Init(); //MD準備(USBシリアルも同時開始)
+  
+  Serial2.begin(115200);  //CAMとのシリアル通信
+  Serial3.begin(115200); //USSとのシリアル通信
+  Serial4.begin(115200); //IMUとのシリアル通信
+
+  while (!Serial1 || !Serial2 || !Serial3 || !Serial4) { //接続待機
+    ;
+  }
+
+  tone(buzzer, 1046.502, 1000); //ド6
+  delay(1000);
+  noTone(buzzer);
+  delay(250);
+  tone(buzzer, 1318.510, 150); //ミ6
+  delay(150);
+  noTone(buzzer);
+  delay(100);
+  tone(buzzer, 1396.913, 250); //ファ6
+  delay(250);
+  noTone(buzzer);
+  delay(150);
+  tone(buzzer, 1567.982, 150); //ソ6
+  delay(150);
+  noTone(buzzer);
+  delay(100);
+  tone(buzzer, 1760.000, 300); //ラ6
+  delay(300);
+  noTone(buzzer);
+  delay(100);
+  tone(buzzer, 1567.982, 250); //ソ6
+  delay(250);
+  noTone(buzzer);
+  delay(200);
+  tone(buzzer, 1567.982, 250); //ソ6
+  delay(250);
+  noTone(buzzer);
+  delay(200);
+  tone(buzzer, 2093.005, 250); //ド7
+  delay(250);
+  noTone(buzzer);
+
+  lcd.clear(); //LCD表示の全削除
 }
 
 void loop() {
@@ -25,6 +82,7 @@ void loop() {
     Serial_receive();
     control_LED();
     pid();
+
     LINE.timer = millis() - LINE.timer_start;
     if (LINE.timer < 300) {
       Motor(1);  //方向修正
@@ -107,11 +165,13 @@ void loop() {
     Serial_receive();
     control_LED();
     pid();
+
     Motor(1);
   } else {
     Serial_receive();
     print_LCD();
     control_LED();
+    
     dsr1202.move(0, 0, 0, 0);
   }
 }
