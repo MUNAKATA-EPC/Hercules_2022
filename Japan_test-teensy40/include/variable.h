4b645e7a661a7f5801665a8bb5a7d6fdbae11fc4
Tatsuya Nakamoto <tatsuyanakamoto1015@gmail.com>
2022-04-02 16:58:01 +0900

    整理

--- Japan_test-teensy40/include/variable.h
+++ Japan_test-teensy40/include/variable.h
@@ -1,33 +1,27 @@
-int head_CAM, CAM_angle, CAM_distance;  //OpenMV 
+int head_CAM, CAM_angle, CAM_distance, CAM_FieldAngle;  //OpenMV 
 
 int head_USS, USS, USS1, USS2, USS3, USS4;  //USS
 
-int IMU = 90; //IMU値
+int IMU; //IMU値
 
 class Status {
-private:
-  /* private何もわからん */
 public:
-  int val = 0;
-  int old_val = 0;
-  int state = 0;
+  int val, old_val;
+  int state;
 };
 
 class Timer {
-private:
-  /* マジでprivate何もわからん */
 public:
-  unsigned long timer = 0;
-  unsigned long timer_start = 0;
+  unsigned long timer, timer_start;
 };
 
 Status LCD, white;
 Timer LINE, position;
 
-int val_I = 0;
-int deviation, old_deviation, val_D = 0;
-float operation_A, operation_B, operation_C = 0;
+int val_I;
+int deviation, old_deviation, val_D;
+float operation_A, operation_B, operation_C;
 
-int motor[] = {0, 0, 0, 0};
+int motor[4];
 
-int MotorPower[] = {0, 0, 0, 0};  //最終操作量
+int MotorPower[4];  //最終操作量
