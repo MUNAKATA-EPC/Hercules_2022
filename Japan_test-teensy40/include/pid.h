8cab720eef5b86bb934db039e5cc1deae5566e3c
Tatsuya Nakamoto <tatsuyanakamoto1015@gmail.com>
2022-04-08 21:43:52 +0900

    PD制御調整

--- Japan_test-teensy40/include/pid.h
+++ Japan_test-teensy40/include/pid.h
@@ -1,16 +1,12 @@
 void pid() {
   deviation = IMU - IMU_center;  //偏差:現在角度と目標角度のズレ
 
-  if (IMU >= 88 && IMU <= 92) {
-    deviation = 0;
-  }
-  
   val_I += deviation;  //偏差の積分値
 
-  if (val_I < -300) {
-    val_I = -300;
-  } else if (val_I > 300) {
-    val_I = 300;
+  if (val_I < -30000) {
+    val_I = -30000;
+  } else if (val_I > 30000) {
+    val_I = 30000;
   }
 
   if (IMU >= 88 && IMU <= 92) {
@@ -25,7 +21,6 @@ void pid() {
   operation_C = gain_D * val_D;  //微分ゲイン*偏差の微分値
 
   operation_A = operation_A + operation_B + operation_C;  //最終的な操作量
-  operation_A = operation_A / operation_PID;  //ゲインを1未満で設定できるように、共通分母で割る(通常ゲインは1以上で設定できない)
 
   if (operation_A < -set_limit_operation) {
     operation_A = -set_limit_operation;  //下限値
--- Japan_test-teensy40/include/pid_parameter.h
+++ Japan_test-teensy40/include/pid_parameter.h
@@ -1,8 +1,7 @@
 int IMU_center = 90; //ジャイロ中心値
-#define gain_P 0.95  //比例ゲイン
+#define gain_P 1.14  //比例ゲイン
 #define gain_I 0  //積分ゲイン
-#define gain_D 6.65  //微分ゲイン
-#define operation_PID 1 //操作分母量
+#define gain_D 119.93  //微分ゲイン
 
 #define set_limit_operation 20  //操作量限界値(絶対値)
 #define motor_speed 24  //モーター速度(絶対値)
