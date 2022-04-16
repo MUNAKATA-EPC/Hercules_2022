d45e9596fca19c5ac9ad6a3e7253eaf4ae176af1
Tatsuya Nakamoto <tatsuyanakamoto1015@gmail.com>
2022-04-17 02:07:17 +0900

    PIDパラメータ確定

--- Japan_test-teensy40/include/pid_parameter.h
+++ Japan_test-teensy40/include/pid_parameter.h
@@ -1,8 +1,8 @@
 int IMU_center = 90; //ジャイロ中心値
 #define gain_P 1.14  //比例ゲイン
-#define gain_I 0  //積分ゲイン
+#define gain_I 0.00  //積分ゲイン
 #define gain_D 119.93  //微分ゲイン
 
 #define set_limit_operation 20  //操作量限界値(絶対値)
-#define motor_speed 24  //モーター速度(絶対値)
+#define motor_speed 27  //モーター速度(絶対値)
 #define motor_limit 40  //モーター速度限界値(絶対値)
