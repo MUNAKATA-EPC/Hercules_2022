8fceecf0b2dcf0416f41eaea1cef12ecebbe1669
Tatsuya Nakamoto <tatsuyanakamoto1015@gmail.com>
2022-07-20 16:08:53 +0900

    文字の色変わる仕様、やっぱりなしの方向で

--- VoltageCheck-m5stick-c/src/main.cpp
+++ VoltageCheck-m5stick-c/src/main.cpp
@@ -13,11 +13,5 @@ void setup() {
 void loop() {
   vbat = M5.Axp.GetVbatData() * 1.1 / 1000;
   M5.Lcd.setCursor(0, 15);
-  if (M5.Axp.GetWarningLeve() == HIGH) {
-    M5.Lcd.setTextColor(RED);
-    M5.Lcd.printf("Volt:%.2fV\n", vbat);
-    M5.Lcd.setTextColor(WHITE);
-  } else {
-    M5.Lcd.printf("Volt:%.2fV\n", vbat);
-  }
+  M5.Lcd.printf("Volt:%.2fV", vbat);
 }
