7e3e904b561020b3f1099fc8820c88005934ec34
Tatsuya Nakamoto <tatsuyanakamoto1015@gmail.com>
2022-07-20 16:11:33 +0900

    GyroController用受信側Picoのプログラム

---
+++ M5Stamp_Pico_Slave_Gyro/M5Stamp_Pico_Slave_Gyro.ino
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <BluetoothSerial.h>
+
+#define LED 32
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("Slave");  //自分の名前指定
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  if (SerialBT.available()) {
+    digitalWrite(LED, HIGH);
+    Serial.write(SerialBT.read());
+  } else {
+    digitalWrite(LED, LOW);
+  }
+}
